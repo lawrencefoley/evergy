a2dfec15e40095e0ed0b152ec7b8f9ac157815a3
Lawrence Foley <emwav333@gmail.com>
2019-02-23 18:45:47 -0600

    Fixes #1. Upgrade requests to fix CVE-2018-18074.

--- kcpl/kcpl.py
+++ kcpl/kcpl.py
@@ -8,32 +8,42 @@ logging.basicConfig(format="%(asctime)s - %(levelname)s - %(name)s -  - %(messag
 class KCPL():
     def __init__(self, username, password):
         self.loggedIn = False
-        self.session = requests.Session()
+        self.session = None
         self.username = username
         self.password = password
-        self.loginUrl = "https://ala.kcpl.com/ala/login.cfm"
-        self.logoutUrl = "https://ala.kcpl.com/ALA/logout.cfm"
-        self.selectMeterUrl = "https://ala.kcpl.com/ala/scrDailyUse.cfm"
-        self.usageUrl = "https://ala.kcpl.com/ALA/scrDailyUse.cfm"
+        self.accountNumber = None
+        self.premiseId = None
+        self.loginUrl = "https://www.kcpl.com/log-in"
+        self.logoutUrl = "https://www.kcpl.com/logout"
+        self.accountSummaryPageUrl = "https://www.kcpl.com/ma/my-account/account-summary/single-account"
+        self.accountDashboardUrl = "https://www.kcpl.com/api/account/{accountNum}/dashboard/current"
+        self.reportUrl = "https://www.kcpl.com/api/account/{accountNum}/dashboard/{year}{monthTwoDigit}/report"
 
     def login(self):
+        self.session = requests.Session()
         logging.info("Logging in with username: " + self.username)
-        loginPayload = {"username": str(self.username), "password": str(self.password)}
+        loginPayload = {"Username": str(self.username), "Password": str(self.password)}
         r = self.session.post(url=self.loginUrl, data=loginPayload, allow_redirects=False)
-        logging.debug("Response Location header: " + str(r.headers["Location"]))
-        if "login" in r.headers["Location"]:
+        logging.debug("Login response: " + str(r.status_code))
+        r = self.session.get(self.accountSummaryPageUrl)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        accountData = soup.find_all('script', id='account-landing-data')
+        if len(accountData) == 0:
             self.loggedIn = False
-            logging.error("Error logging in, was redirect to login page")
         else:
-            logging.debug("Login response: " + str(r.status_code))
-            self.loggedIn = True
+            self.accountNumber = json.loads(accountData[0].text)["accountNumber"]
+            dashBoardData = self.session.get(self.accountDashboardUrl.replace("{accountNum}", self.accountNumber)).json()
+            self.premiseId = dashBoardData["addresses"][0]["premiseId"]
+            self.loggedIn = self.accountNumber is not None and self.premiseId is not None
 
     def logout(self):
         logging.info("Logging out")
-        r = self.session.get(url=self.loginUrl)
+        r = self.session.get(url=self.logoutUrl)
+        self.session = None
         self.loggedIn = False
 
     def getMeters(self):
+        logging.warn("getMeters() has been deprecated! KCPL removed meters from their website and API and now just rolls all meters up into a total")
         if not self.loggedIn:
             logging.error("Must login first")
             return
@@ -50,47 +60,31 @@ class KCPL():
                 meters.append(item.text)
             return meters
 
-    def getUsage(self, meter):
+    def getUsage(self):
         if not self.loggedIn:
             logging.error("Must login first")
             return
-        meterPayload = {"selectedMeter": str(meter)}
-        r = self.session.post(self.usageUrl, meterPayload)
-
-        soup = BeautifulSoup(r.text, 'html.parser')
-        data = []
-        tables = soup.find_all(id="example")
-        if len(tables) < 1:
-            logging.debug("Error retrieving data")
-            return 
-        for row in tables[0].find_all('tr'):
-            cols = row.find_all('td')
-            if len(cols) > 0:
-                datum = {}
-                datum["date"] = cols[1].text
-                datum["reading"] = cols[2].text
-                datum["kwh"] = cols[3].text
-                datum["cost"] = cols[7].text
-                data.append(datum)
-        return data
+        # TODO Allow querying for month instead of day
+        usageData = self.session.get("https://www.kcpl.com/api/report/usage/" + self.premiseId + "?interval=d").json()
+        return usageData["data"]
 
 def getCreds():
     with open("../credentials.json", 'r') as f:
         return json.loads(f.read())
 
 if __name__ == "__main__":
+    # Read the credentials.json file
     creds = getCreds()
     username = creds["username"]
     password = creds["password"]
 
     kcpl = KCPL(username, password)
     kcpl.login()
-    # Get a list of the meters for your account
-    meters = kcpl.getMeters()
-    logging.info("Meters: " + str(meters))
-    logging.info("Using first meter: " + str(meters[0]))
-    # Get readings for a meter
-    data = kcpl.getUsage(meters[0])
-    logging.info("Last meter reading: " + str(data[0]))
+
+    # Get a list of daily readings
+    data = kcpl.getUsage()
+    logging.info("Last usage data: " + str(data[-1]))
+    logging.info("Last usage reading: " + str(data[-1]["usage"]))
+
     # End your session by logging out
     kcpl.logout()
