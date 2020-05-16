abef451259016c299178059dd49b5b9bbee4132a
Patrick McDonagh <patrickjmcd@gmail.com>
2020-05-16 16:20:13 -0500

    updates to work with Evergy API

--- kcpl/kcpl.py
+++ kcpl/kcpl.py
@@ -1,9 +1,14 @@
 import json
 import requests
 from bs4 import BeautifulSoup
+from datetime import date, timedelta
 import logging
 
 logging.basicConfig(format="%(asctime)s - %(levelname)s - %(name)s -  - %(message)s", level=logging.INFO)
+
+day_before_yesterday = date.today() - timedelta(days=2)
+yesterday = date.today() - timedelta(days=1)
+today = date.today()
 
 class KCPL():
     def __init__(self, username, password):
@@ -13,59 +18,44 @@ class KCPL():
         self.password = password
         self.accountNumber = None
         self.premiseId = None
-        self.loginUrl = "https://www.kcpl.com/log-in"
-        self.logoutUrl = "https://www.kcpl.com/logout"
-        self.accountSummaryPageUrl = "https://www.kcpl.com/ma/my-account/account-summary/single-account"
-        self.accountDashboardUrl = "https://www.kcpl.com/api/account/{accountNum}/dashboard/current"
-        self.reportUrl = "https://www.kcpl.com/api/account/{accountNum}/dashboard/{year}{monthTwoDigit}/report"
+        self.loginUrl = "https://www.evergy.com/log-in"
+        self.logoutUrl = "https://www.evergy.com/logout"
+        self.accountSummaryUrl = "https://www.evergy.com/ma/my-account/account-summary"
+        self.accountDashboardUrl = "https://www.evergy.com/api/account/{accountNum}/dashboard/current"
+        self.usageDataUrl = "https://www.evergy.com/api/report/usage/{premiseId}?interval={query_scale}&from={start}&to={end}"
 
     def login(self):
         self.session = requests.Session()
         logging.info("Logging in with username: " + self.username)
-        loginPayload = {"Username": str(self.username), "Password": str(self.password)}
+        loginPayload = {"username": str(self.username), "password": str(self.password)}
         r = self.session.post(url=self.loginUrl, data=loginPayload, allow_redirects=False)
         logging.debug("Login response: " + str(r.status_code))
-        r = self.session.get(self.accountSummaryPageUrl)
+        r = self.session.get(self.accountSummaryUrl)
         soup = BeautifulSoup(r.text, 'html.parser')
         accountData = soup.find_all('script', id='account-landing-data')
         if len(accountData) == 0:
             self.loggedIn = False
         else:
-            self.accountNumber = json.loads(accountData[0].text)["accountNumber"]
-            dashBoardData = self.session.get(self.accountDashboardUrl.replace("{accountNum}", self.accountNumber)).json()
-            self.premiseId = dashBoardData["addresses"][0]["premiseId"]
+            self.accountNumber = json.loads(accountData[0].contents[0])["accountNumber"]
+            dashboardData = self.session.get(self.accountDashboardUrl.format(accountNum=self.accountNumber)).json()
+            self.premiseId = dashboardData["addresses"][0]["premiseId"]
             self.loggedIn = self.accountNumber is not None and self.premiseId is not None
 
     def logout(self):
         logging.info("Logging out")
-        r = self.session.get(url=self.logoutUrl)
+        self.session.get(url=self.logoutUrl)
         self.session = None
         self.loggedIn = False
 
-    def getMeters(self):
-        logging.warn("getMeters() has been deprecated! KCPL removed meters from their website and API and now just rolls all meters up into a total")
-        if not self.loggedIn:
-            logging.error("Must login first")
-            return
-        meters = []
-        r = self.session.get(self.selectMeterUrl)
-        soup = BeautifulSoup(r.text, 'html.parser')
-        picklist = soup.find_all("select", {"name":"selectedMeter"})
-        if len(picklist) == 0:
-            logging.debug("Could not find meter list")
-            return meters
-        else:
-            picklistItems = picklist[0].find_all("option")
-            for item in picklistItems:
-                meters.append(item.text)
-            return meters
 
-    def getUsage(self):
+    def getUsage(self, start=day_before_yesterday, end=yesterday, query_scale="d"):
+        """Fetches all usage data from the given period."""
         if not self.loggedIn:
             logging.error("Must login first")
             return
-        # TODO Allow querying for month instead of day
-        usageData = self.session.get("https://www.kcpl.com/api/report/usage/" + self.premiseId + "?interval=d").json()
+        url = self.usageDataUrl.format(premiseId=self.premiseId, query_scale=query_scale, start=start, end=end)
+        logging.debug("fetching {}".format(url))
+        usageData = self.session.get(url).json()
         return usageData["data"]
 
 def getCreds():
@@ -82,6 +72,7 @@ if __name__ == "__main__":
     kcpl.login()
 
     # Get a list of daily readings
+
     data = kcpl.getUsage()
     logging.info("Last usage data: " + str(data[-1]))
     logging.info("Last usage reading: " + str(data[-1]["usage"]))
